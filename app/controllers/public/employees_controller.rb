abea68d6be72e6b27bfb78f82e5ee7b9ed838306
Rodrigo Rodriguez <rrodriguez@growthaccelerationpartners.com>
2013-01-03 12:46:38 -0600

    Adding few changes and removing an exception. Algo updating Rails version.

--- app/controllers/public/employees_controller.rb
+++ app/controllers/public/employees_controller.rb
@@ -7,11 +7,12 @@ class Public::EmployeesController < ApplicationController
 
   # GET /employees/new
   def search_result_details
-    @employee = Employee.find_by_identification(params[:employee][:identification])
-    @vacations = @employee.vacations.where("status != 2").order('requested_on DESC')
+    @employee = Employee.find_by_identification(params[:employee][:identification].gsub('\D', ''))
+    @vacations = @employee.vacations.where("status != 2").order('requested_on DESC') if @employee
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @employee }
     end
+    
   end
 end
